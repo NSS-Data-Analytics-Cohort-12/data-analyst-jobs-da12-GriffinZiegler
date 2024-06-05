402d5505341e01914a08e08d7c1c51ad774c1624
Griffin Ziegler <griffintyziegler@gmail.com>
2024-06-04 21:15:58 -0400

    Question 6

--- scripts/READMEanswers.sql
+++ scripts/READMEanswers.sql
@@ -45,6 +45,14 @@ WHERE review_count BETWEEN 500 AND 1000;
 
 -- 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
 
+SELECT AVG(star_rating) AS avg_rating, location AS state
+FROM data_analyst_jobs
+	WHERE star_rating IS NOT NULL
+GROUP BY location
+ORDER BY avg_rating DESC;
+
+-- NE
+
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
 -- 8.	How many unique job titles are there for California companies?
