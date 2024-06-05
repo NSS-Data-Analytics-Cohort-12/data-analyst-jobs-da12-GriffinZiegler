4f2ae6fa6dc4210619de4a218fef433538e5ac39
Griffin Ziegler <griffintyziegler@gmail.com>
2024-06-04 21:43:55 -0400

    Question 11

--- scripts/READMEanswers.sql
+++ scripts/READMEanswers.sql
@@ -78,7 +78,22 @@ HAVING review_count > 5000;
 
 -- 10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
--- 11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+SELECT company, avg(star_rating), review_count
+FROM data_analyst_jobs
+	WHERE company IS NOT NULL
+GROUP BY company, review_count
+HAVING review_count > 5000
+ORDER BY avg(star_rating) DESC, review_count DESC;
+
+-- Kaiser Permanente, 4.199 8164 review counts
+
+-- 11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+
+SELECT count(DISTINCT title)
+FROM data_analyst_jobs
+WHERE title LIKE ('%Analyst%');
+
+-- 754
 
 -- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 
