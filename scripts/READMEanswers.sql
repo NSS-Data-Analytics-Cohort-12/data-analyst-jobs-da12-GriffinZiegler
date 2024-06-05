a35e3134849d7e9872a9ed5e2c5806d0a0963d98
Griffin Ziegler <griffintyziegler@gmail.com>
2024-06-04 21:20:42 -0400

    Question 8

--- scripts/READMEanswers.sql
+++ scripts/READMEanswers.sql
@@ -62,6 +62,12 @@ FROM data_analyst_jobs;
 
 -- 8.	How many unique job titles are there for California companies?
 
+SELECT COUNT(distinct title)
+FROM data_analyst_jobs
+WHERE location = 'CA';
+
+-- 230
+
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
 -- 10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
